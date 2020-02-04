7e63d0a7e21625bc59e0f8d140bb9d9f98999a64
Finneasp2020 <45699398+Finneasp2020@users.noreply.github.com>
2020-02-04 12:36:43 -0800

    My first Unity assignment
    It was hard, but I definitely have a better understanding of how Unity and GitHub work now.

---
+++ First project (hopefully)/Assets/cylinderScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using charset = 'UTF-8';
+
+public class cylinderScript : MonoBehaviour
+
+{
+    public GameObject cylinder;
+    //private GameObject cylinder;
+    private Vector3 scaleChange;
+    // Start is called before the first frame update
+    public GameObject sphere;
+    public Vector3 startScaling = new Vector3(-2, 0, 0);
+    void Start()
+    {
+    sphere = GameObject.Find("Sphere");
+    scaleChange = new Vector3(-0.01f, -0.01f, -0.01f);
+}
+
+// Update is called once per frame
+void Update()
+    {
+        if (sphere.transform.position.x >= startScaling.x)
+        {
+            transform.localScale += scaleChange;
+        }
+            
+        if (transform.localScale.y < 0.1f || transform.localScale.y > 1.0f)
+        {
+            scaleChange = -scaleChange;
+        }
+    }
+}
